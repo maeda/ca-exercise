2abd3c70dcc0bfb7301d46f0f021ee7f47250e05
Andherson Maeda <maeda.br@gmail.com>
2021-07-20 23:39:48 -0300

    Adding validation to avoid store events from the future @maeda

--- eye/serializers.py
+++ eye/serializers.py
@@ -1,3 +1,4 @@
+import datetime
 from rest_framework import serializers
 
 from eye.models import Event
@@ -11,3 +12,13 @@ class EventSerializer(serializers.HyperlinkedModelSerializer):
     def create(self, validated_data):
         entity, is_created = Event.objects.update_or_create(**self.validated_data)
         return entity
+
+    def validate(self, attrs):
+        if 'timestamp' in attrs:
+            timestamp = attrs['timestamp']
+            now = datetime.datetime.now(tz=timestamp.tzinfo)
+            if attrs['timestamp'] > now:
+                raise serializers.ValidationError({
+                    'timestamp': 'timestamp date cannot be greater and current datetime.'
+                })
+        return super(EventSerializer, self).validate(attrs)
