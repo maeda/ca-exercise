871a1d9b58a1633bccbe6eb4b09a084d290f05df
Andherson Maeda <maeda.br@gmail.com>
2021-07-21 00:20:39 -0300

    Removing unused code and libraries @maeda

--- eye/urls.py
+++ eye/urls.py
@@ -7,5 +7,4 @@ router.register(r'event', views.ListEvents)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
